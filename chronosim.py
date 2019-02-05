7bccf25ef503782916e567b779d8d2328b79ff60
Jonathan Guiang <jkguiang@gmail.com>
2019-02-04 16:14:05 -0800

    fixed propagation issue

--- chronosim.py
+++ chronosim.py
@@ -11,31 +11,30 @@ def DotProduct(a, b):
 
 def CrossProduct(a, b):
     """ Return a x b """
-    cross = []
-    cross.append(a[1]*b[2] - a[2]*b[1])
-    cross.append(a[2]*b[0] - a[0]*b[2])
-    cross.append(a[0]*b[1] - a[1]*b[0])
-    return cross
+    return [ (a[1]*b[2] - a[2]*b[1]), (a[2]*b[0] - a[0]*b[2]), (a[0]*b[1] - a[1]*b[0]) ]
 
 def GetTransform(n):
     """ Get matrix R that rotates point into new basis
         where z -> n (normalized facet normal vector)
     """
     # New basis vectors in original coordinates
-    e2 = n                    # z -> normal-direction (originally in x-y)
-    e1 = [0,0,1]              # y -> z-direction
-    e0 = CrossProduct(e1, e2) # x -> e1 = e1xe2
+    e3 = n
+    if n[2] != 0:
+        e2 = [0,1,0]              
+    else:
+        e2 = [0,0,1]
+    e1 = CrossProduct(e2, e3)
     # Construct rotation matrix
-    R = [ [e0[0], e1[0], e2[0]], 
-          [e0[1], e1[1], e2[1]], 
-          [e0[2], e1[2], e2[2]] ]
+    R = [ [e1[0], e1[1], e1[2]], 
+          [e2[0], e2[1], e2[2]], 
+          [e3[0], e3[1], e3[2]] ]
 
-    return np.array(R)
+    return R
 
 def CheckHit(poly, hitPos, norm):
     """ Check that nearby ray's trajectory intersects polygon """
     # Return False if hit pos on opposite side of BTL
-    if DotProduct([hitPos[0], hitPos[1], 0], norm) < 0:
+    if DotProduct(hitPos, norm) < 0:
         return False
     # Stores vectors from hit pos to each vertex of poly
     hitPosToVertex = []
@@ -43,8 +42,8 @@ def CheckHit(poly, hitPos, norm):
     R = GetTransform(norm)
     for vertex in poly:
         # Get hit pos and vertex in rotated basis
-        hitPosTransf = np.matmul(R, np.array(hitPos))
-        vertexTransf = np.matmul(R, np.array(vertex))
+        hitPosTransf = np.matmul(R, hitPos)
+        vertexTransf = np.matmul(R, vertex)
         # Translate hit pos and vertex to same plane
         hitPosTransf[2] = 0
         vertexTransf[2] = 0
@@ -60,12 +59,11 @@ def CheckHit(poly, hitPos, norm):
 
 def Parse(layers, norms, rays, outPath, verbose=False):
     """ Parse polygons and rays, look for hits """
-    from tqdm import tqdm
     # Setup output file
     outFile = ROOT.TFile(outPath, "RECREATE")
     rayOut = OutTree()
     # Loop over rays
-    for i, ray in enumerate(tqdm(rays)):
+    for i, ray in enumerate(rays):
         # Initialize output tree
         rayOut.GetKinematics(ray)
         nHits = 0
@@ -73,13 +71,10 @@ def Parse(layers, norms, rays, outPath, verbose=False):
         layerHitPosX = [ 0 for l in layers ]
         layerHitPosY = [ 0 for l in layers ]
         layerHitPosZ = [ 0 for l in layers ]
+        # Get (potential) hit position
+        hitPos = ray["pos"]
         # Loop over layers
         for j, layer in enumerate(layers):
-            # Get particle's position at layer's z-level
-            t = abs(layer[0][0][2] - ray["pos"][2])/ray["p"][2]
-            hitPos = [ ray["pos"][0]+t*ray["p"][0], 
-                       ray["pos"][1]+t*ray["p"][1],
-                       ray["pos"][2]+t*ray["p"][2] ]
             # Save first position for all particles for efficiency calculation
             if j == 0:
                 rayOut.x = hitPos[0]
